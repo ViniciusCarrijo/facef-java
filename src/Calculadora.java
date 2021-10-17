9846e88d3578113255ec5ee71b9207c97f90bfd0
Vinicius Carrijo <viniciuscarrijo@hotmail.com>
2021-10-17 20:46:57 -0300

    exercicio 1 ao 10 da lista

---
+++ src/Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+    public static Double somar(Double valor1, Double valor2) {
+        return valor1 + valor2;
+    }
+    public static Double subtrair(Double valor1, Double valor2) {
+        return valor1 - valor2;
+    }
+    public static Double dividir(Double valor1, Double valor2) {
+        return valor1 / valor2;
+    }
+    public static Double multiplicar(Double valor1, Double valor2) {
+        return valor1 * valor2;
+    }
+}
---
+++ src/CalculadoraComercial.java
@@ -0,0 +1,6 @@
+public class CalculadoraComercial extends Calculadora{
+
+    public static Double porcentagem (Double valorTotal, Double porcentagem) {
+        return (valorTotal * porcentagem) / 100;
+    }
+}
